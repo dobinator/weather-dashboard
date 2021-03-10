a9096e087b4eab1ca514166946dcbcb74bf6e3a1
grey cashmere <dobsonemily@gmail.com>
2021-03-10 15:40:00 -0700

    fixed bad readme file

--- assets/script.js
+++ assets/script.js
@@ -3,46 +3,64 @@
 var api_key = "452c3222c520d5a1ef11ff57192c158b"; 
 var btns = document.querySelector("#buttons");
 var form = document.querySelector("#weather-form");
-var lat = weather.coord.lat;
-var lon= weather.coord.lon;
 var cities= localStorage.getItem("cities")
+
+if (cities){
+    cities = JSON.parse(cities)
+}else {
+    cities = []; 
+}
+
+function renderCityBtns(){
+    cities.forEach (function(city){
+        //build a btn
+        var btn= document.createElement("button");
+        //set its text to be the city name
+        var btn= document.createElement("button");
+        //append the btn to the city-btn div
+    })
+}
+
+
 
 
 // event Listener for our form submit
+
 form.addEventListener("submit", function (e){
- var city = document.querySelector("#city-input").value; 
-
- e.preventDefault();
- getWeather(city);
-
+    e.preventDefault();
+    var city = document.querySelector("#city-input").value; 
+    
+    getWeather(city);
 }); 
 
 //Listen to our users click on the button, call the function of the event
 btns.addEventListener("click", function(e){
-  //stopping the search if the button has already populated the city
-  e.preventDefault();
-
-  if(!e.target.matches("button")) return;
-
-  var city= e.target.textContent;
-  getWeather(city); 
+    //stopping the search if the button has already populated the city
+    e.preventDefault();
+    
+    if(!e.target.matches("button")) return;
+    
+    var city= e.target.textContent;
+    getWeather(city); 
 });
 
 
 //function takes city name and retrieves weather data for that city
 function getWeather(city){
-
+    
     var currentWeatherUrl= `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
-
-
-//send fetch request to get latitude and longitude
-fetch(currentWeatherUrl)
-.then((data)=>data.json())
-.then(function (weather){
-if(weather.cod === "404") {
-    alert("City not found");
-    return;
-}
+    
+    var lat = weather.coord.lat;
+    var lon= weather.coord.lon;
+    
+    //send fetch request to get latitude and longitude
+    fetch(currentWeatherUrl)
+    .then((data)=>data.json())
+    .then(function (weather){
+        if(weather.cod === "404") {
+            alert("City not found");
+            return;
+        }
 if (!cities.includes(weather.name)){
     cities.push(weather.name);
     localStorage.setItem("cities,", JSON.stringify(cities));
