a13edf0253015f4ce1995dda1d8c0e9f30d59e83
grey cashmere <dobsonemily@gmail.com>
2021-03-11 08:57:43 -0700

    updating js for weather

--- assets/script.js
+++ assets/script.js
@@ -1,69 +1,68 @@
 //api key for app to work
-var api_key = "452c3222c520d5a1ef11ff57192c158b"; 
+const api_key = "452c3222c520d5a1ef11ff57192c158b"; 
 //activating HTML in JS
-var form = document.querySelector("#weather-form");
-var inputCity= localStorage.getItem("#inputCity");
-var searchBtns = document.getElementById("searchBtn");
-//var cities = localStorage.getItem("cities"); 
+const form = document.querySelector("#weather-form");
+//var inputCity= localStorage.getItem("currentCity");
+// var searchBtns = document.getElementById("searchBtn");
+var inputCity = JSON.parse(localStorage.getItem("cities"))|| [] 
+var searchHistory= document.getElementById ("searchHistory") 
+var todaysWeather= document.getElementById("currentCity")
+var lat = weather.coord.lat;
+var lon= weather.coord.lon;
 
 
-if (inputCity){
-    inputCity = JSON.parse(inputCity)
-}else {
-    inputCity = []; 
-}
-//changing the kelvin to Fahrenheit for the weather load the api . 
-
-function kelvinToFahrenheit(temp){
- return (temp - 273.15) * 1.8 +32;
-
-}
 
 //calling the function to render the city buttons
 function renderCityBtns(){
-    searchBtns.innerHTML = " ";
-    inputCity.forEach(function(city){
-        //build a btn
-        var btn= document.createElement("searchBtn");
+    // searchBtns.innerHTML = " ";
+    console.log(inputCity)
+
+    for( i= 0; i <inputCity.length; i++){
+        var btn = document.createElement("button");
         //set its text to be the city name
-        newBtn= inputCity.value  
+        btn.textContent = inputCity[i]; 
         //append the btn to the city-btn div
-        searchBtns.append(newBtn); 
-    });
+        searchHistory.append(btn); 
+     } 
+
 }
+
+
 renderCityBtns();
 
-
+var search= document.getElementById("searchBtn")
 // event Listener for our form submit
- form.addEventListener("submit", function (e){
+
+ search.addEventListener("click", function (e){
 
     e.preventDefault();
-    var city = document.querySelector("inputCity").value; 
-    
+    var city = document.querySelector("#inputCity").value; 
+    console.log(city)
     getWeather(city);
 }); 
 
 //Listen to our users click on the button, call the function of the event
- searchBtns.addEventListener("click", function(e){
-    //stopping the search if the button has already populated the city
-    e.preventDefault();
+//  searchBtns.addEventListener("click", function(e){
+//     //stopping the search if the button has already populated the city
+//     e.preventDefault();
     
-    if(!e.target.matches("searchBtn")) return;
+//     if(!e.target.matches("searchBtn")) return;
     
-    var city= e.target.textContent;
-    getWeather(city); 
-});
+//     var city= e.target.textContent;
+//     getWeather(city); 
+// });
 
 
 //function takes city name and retrieves weather data for that city
 function getWeather(city){
     
-    var currentWeatherUrl= `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+    var currentWeatherUrl= `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=imperial`;
     
     //send fetch request to get latitude and longitude
     fetch(currentWeatherUrl)
     .then((data)=>data.json())
     .then(function (weather){
+        console.log(weather)
         //alert created if the user enters a city not found
         if(weather.cod === "404") {
             alert("City not found");
@@ -71,13 +70,15 @@ function getWeather(city){
         }// save the user input for name of city 
         if (!inputCity.includes(weather.name)){
             inputCity.push(weather.name);
-            localStorage.setItem("currentCity,", JSON.stringify(city));
+            //checking for the array and pushing to local storage
+            localStorage.setItem("cities", JSON.stringify(inputCity));
             renderCityBtns(); 
         }
-        //lat and long for the api call
-        var lat = weather.coord.lat;
-        var lon= weather.coord.lon;
-        
+
+        //temp for the api call
+        var temp = document.createElement("p").textContent= weather.main.temp
+        todaysWeather.append(temp)
+        var wind = document.createElement("p").textContent= weather.wind_speed
 
 //api call for the latitude and longitude
 var onecallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${api_key}`;
@@ -93,4 +94,4 @@ fetch(onecallURL)
 });
 }
 
-getWeather("tucson");
+//getWeather();
