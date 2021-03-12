8341e00b5fa162f5e60311309504cfc797681a85
grey cashmere <dobsonemily@gmail.com>
2021-03-11 17:32:45 -0700

    updating

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,19 @@
 //api key for app to work
 const api_key = "452c3222c520d5a1ef11ff57192c158b"; 
 //activating HTML in JS
-const form = document.querySelector("#weather-form");
-//var inputCity= localStorage.getItem("currentCity");
-// var searchBtns = document.getElementById("searchBtn");
+var date = new Date();
+displayDate = date.toLocaleString('en-US',{month: 'numeric', day:'2-digit', year:'numeric'});
 var inputCity = JSON.parse(localStorage.getItem("cities"))|| [] 
 var searchHistory= document.getElementById ("searchHistory") 
 var todaysWeather= document.getElementById("currentCity")
 var windSpeed = document.getElementById("windSpeed")
+var search= document.getElementById("searchBtn")
 
-
+// const form = document.querySelector("#weather-form");
 
 //calling the function to render the city buttons
 function renderCityBtns(){
-    // searchBtns.innerHTML = " ";
-    console.log(inputCity)
-    
-    for( i= 0; i <inputCity.length; i++){
+     for( i= 0; i <inputCity.length; i++){
         var btn = document.createElement("button");
         //set its text to be the city name
         btn.textContent = inputCity[i]; 
@@ -26,12 +23,9 @@ function renderCityBtns(){
 }
 renderCityBtns();
 
-
-var search= document.getElementById("searchBtn")
 // event Listener for our form submit
 
 search.addEventListener("click", function (e){
-    
     e.preventDefault();
     var city = document.querySelector("#inputCity").value; 
     console.log(city)
@@ -51,24 +45,26 @@ search.addEventListener("click", function (e){
     
     //function takes city name and retrieves weather data for that city
     function getWeather(city){
+ var currentWeatherUrl= `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=imperial`;
         
-        var currentWeatherUrl= `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=imperial`;
-        
-        //send fetch request to get latitude and longitude
-        fetch(currentWeatherUrl)
-        .then((data)=>data.json())
-        .then(function (weather){
-            console.log(weather)
-            //alert created if the user enters a city not found
-            if(weather.cod === "404") {
-                alert("City not found");
-                return;
-            }// save the user input for name of city 
-            if (!inputCity.includes(weather.name)){
-                inputCity.push(weather.name);
-                //checking for the array and pushing to local storage
-                localStorage.setItem("cities", JSON.stringify(inputCity));
-                renderCityBtns(); 
+ //send fetch request to get latitude and longitude
+    fetch(currentWeatherUrl)
+    .then((data)=>data.json())
+    .then(function (weather){
+    //console.log(weather)
+    //alert created if the user enters a city not found
+    if(weather.cod === "404") {
+    alert("City not found");
+    return;
+    }
+    // save the user input for name of city 
+    if (!inputCity.includes(weather.name)){
+    inputCity.push(weather.name);
+    //checking for the array and pushing to local storage
+    localStorage.setItem("cities", JSON.stringify(inputCity));
+
+
+    renderCityBtns(); 
             }
             var lat = weather.coord.lat;
             var lon= weather.coord.lon;
@@ -84,7 +80,7 @@ search.addEventListener("click", function (e){
         //moment for date and time
 
 //api call for the latitude and longitude
-var onecallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${api_key}`;
+var onecallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${api_key}`;
 fetch(onecallURL)
 .then((data) => data.json()
 .then(function (oneCallData){
