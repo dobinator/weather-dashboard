8d005b129a3da713125f0ce45c01249be09cbce1
grey cashmere <dobsonemily@gmail.com>
2021-03-18 10:03:39 -0700

    adding functionality with dates and icons

--- assets/script2.js
+++ assets/script2.js
@@ -1,75 +1,93 @@
-const api_key = "8c13e3d659618767f6b81c58577f6e75";
+//api key for app to work//
+const api_key = "8c13e3d659618767f6b81c58577f6e75"; 
+// var inputCity = JSON.parse(localStorage.getItem("inputCity"))|| []
+var cityBtn = document.getElementById("citySearchbtn"); 
+var city = ""; 
+var searchCity = document.getElementById("searchHistory"); 
+var cityArray = []; 
+var temp = document.querySelector("#temperature"); 
+var humidity= document.querySelector("#humidity"); 
+var windSpeed = document.querySelector("#windSpeed");
+var uvIndex = document.getElementById("uvIndex");
+var icon = document.querySelector("#icon")
+var forecastIcon = document.getElementById("forecastIcon")
 
-var citySearchBtn= document.querySelector("#citySearchBtn"); 
-var searchCity= document.querySelector("#citySearchForm");
-var temperature = document.querySelector("#temperature");
-var windSpeed= document.querySelector ("#windSpeed"); 
-var humidity = document.querySelector("#humidity");
-var uvIndex = document.querySelector ("#unIndex");
-var searchHistory= document.querySelector("#searchHistory")
-
-
-    function findCity(c) {
-        for (let i = 0; i < cityArray.length; i++) {
-            if (c.toUpperCase() = cityArray[i]) {
-                console.log(city);
-                return -1;
-            }
-            return 1;
+//search for a city to see if it exists
+function findCity(c) {
+    for (let i = 0; i < cityArray.length; i++) {
+        if (c.toUpperCase() = cityArray[i]) {
+            console.log(city);
+            return -1;
         }
+        return 1;
     }
-
-    //Display current weather
-    function getWeather(event) {
-        event.preventDefault();
-        if (searchCity.value.trim !== "" || searchCity.value > 0) {
-            var city = searchCity.value    //.trim removes white spaces beginning and end of a string
-            getcurrentWeather(city);
-        }
-
-    }
-
-
-    citySearchBtn. addEventListener("click", function(e){
-        e.preventDefault()
-
-    function getcurrentWeather(city) {
-        var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
-        fetch(currentWeatherUrl)
-            .then((response) => response.json())
-            .then(data =>{
-                console.log(data);
-                temperature = temperature.innerHTML = "Temperature: "+ Math.floor(((data['main']['temp'] - 273.15) * 1.80 + 32))+" ℉";
-                humidity = humidity.innerHTML = "Humidity: "+ (data['main']['humidity']+"%");
-                windSpeed = windSpeed.innerHTML = "Windspeed: "+ (data['wind']['speed'] .toFixed(1) + "MPH");
+    console.log(c)
+}
 
 
 
-
-
-                //uvIndex = uvIndex.innerHTML = "UV Index: "+ (data['main']['temp'] - 273.15)
-                // //if error code is 404 display "City Not Found"
-                // if (data[cod] == "404") {
-                //     alert("City not found!");
-                //     return; //exits the function & returns the value of the error
-                // }
-                // if (data[cod]== "400") {
-                //     alert("Please put the name of a city to search first!");
-                //     return; //exits the function & returns the value of the error
-                // }
-                var latitude = data['coord']['lat'];
-                console.log(latitude)
-                var longitude = data['coord']['lon'];
-                console.log(longitude)
-                var oneCallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&appid=${api_key}`
+function getWeather(city) {
+    var currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+    fetch(currentWeatherURL)
+    .then((response) => response.json())
+    .then(data => {
+        console.log(data);
+        temp = temp.innerHTML = "Temperature: "+ Math.floor(((data['main']['temp']- 273.15) * 1.80 + 32))+" ℉";
+        humidity = humidity.innerHTML = "Humidity: "+ (data['main']['humidity']+"%");
+        windSpeed = windSpeed.innerHTML = "Windspeed: "+ (data['wind']['speed'] .toFixed(1)+ "MPH");
+        // icon = icon.innerHTML = "src",` http://openweathermap.org/img/w`+ data.weather[0].icon+ ".png"; 
+        icon.setAttribute("src", `http://openweathermap.org/img/w/`+ data.weather[0].icon+ ".png")
+                var lat = data['coord']['lat'];
+                var lon = data['coord']['lon'];
+              
+                
+     uvIndexValue(lat, lon)          
+     fiveDay(city)
             
             });
+        }
 
+        function uvIndexValue(lat,lon){
+        var oneCallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${api_key}`
+        fetch(oneCallURL) 
+        .then((response) => response.json())
+        .then(data => {
+         uvIndex = uvIndex.innerHTML = "UV Index: "+ (data.current.uvi)
+
+            console.log(data)
+          
+        }) 
+        }
+    var id = 1
+    function fiveDay (city){
+     var getFiveDay= `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}&units=imperial`; 
+    fetch(getFiveDay)
+    .then((data)=>data.json())
+    .then(function (data){
+        console.log(data)
+           var currentDay= data.list
+            for(let index= 0; index < currentDay.length; index++){
+            var today = currentDay[index]
+            if (today.dt_txt.indexOf ("09:00:00")!== -1){
+            var date= document.getElementById (`date${id}`)
+            date.textContent = today.dt_txt.slice(0,9)
+            var img= document.getElementById(`forecastIcon${id}`)
+            img.setAttribute ("src", `http://openweathermap.org/img/w/`+ today.weather[0].icon+ ".png")
+            id++
+            }
+            }
+  
+                    
+    })}
+
+
+ cityBtn.addEventListener ("click", function(e){
+    e.preventDefault();
+    if (searchCity.value.trim !== "" || searchCity.value > 0) {
+        var city = searchCity.value//.trim removes white spaces beginning and end of a string
+        getWeather(city);
+        console.log(city);
     }
-    getcurrentWeather()
-
-   // function getFiveDay(city){
-       // var getFiveDay= `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=imperial`;   
 
 
-}; 
+}); 
